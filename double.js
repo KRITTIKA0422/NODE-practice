748cb000742f39bb5100c0af6af87fa59787ac96
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-07-22 22:24:29 +0530

    code draft

--- double.js
+++
@@ -1,3 +0,0 @@
-console.log(process.argv);
-const dbl=(num)=>num*2;
-console.log(dbl(process.argv[2]));
--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  import express from "express";
  const app=express();
  const PORT=4000;
-
- app.get("/files", async function(request,response){
+ var fileList=[];
+ app.get("/files", function(request,response){          //GET request for API endpoint for creating a file 
   let {time,file}=TimeStamp();
-   fs.writeFile(`./Clock/${file}.txt`,time,(err)=> {
+   fs.writeFile(`./Clock/${file}.txt`,time,(err)=> {          //creating file under folder Clock
     if(err){
       response.send(err);
     }
@@ -13,21 +13,28 @@
     response.send(`File created with file name ${file}`);
     }
    })
+   fileList.push(`Filename ${file}`);                         //storing the created file names in an array
+  });
+  app.get("/filelist", function(request,response){             //GET request for API endpoint to retrieve the created filenames 
+    response.send(fileList);
   });
   function TimeStamp(){
-  var d = Date.now();
-  var date = new Date(d);
-  var day = date.getDate();
-  var month = date.getMonth() +1;
-  var year = date.getFullYear();
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var seconds = date.getSeconds();
-  var time= hours+":"+minutes+":"+seconds;
-  var file = day+"."+month+"."+year+"-"+hours+":"+minutes+":"+seconds;   
-  return {time,file};
-   }
-   
+    var d = Date.now();
+    var date = new Date(d);
+    var day = date.getDate();
+    var month = date.getMonth() +1;
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var time= hours+":"+minutes+":"+seconds;           //to print the current time stamp inside the file
+    var file = day+"-"+month+"-"+year+"_"+hours+"_"+minutes+"_"+seconds;   //to give file name
+    return {time,file};
+     }
+     const urlDetails="Heroku URL for creating a file- "+"\n"+"Heroku URL for retrieving text files created in a folder- "+"\n"+"Github repository link-https://github.com/KRITTIKA0422/nodejs-filesystem"+"\n"+"Last committed hash ID- ";
+    fs.writeFile("./details.txt",urlDetails,(err)=> {          //creating file having heroku url, github repository, last committed hash id
+    console.log("Details of url are written");
+     });
 app.listen(PORT,()=>console.log(`App started in ${PORT}`));
 
 
